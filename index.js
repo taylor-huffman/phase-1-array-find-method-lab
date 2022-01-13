79e6f9cb8869d1bd34aa3444987d968e9d1ca51c
Taylor Huffman <taylor@crucialartists.com>
2022-01-13 16:27:28 -0600

    Began assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+
+function superbowlWin(array) {
+    let foundItem = (array.find(element => element.result === "W"))
+    if (foundItem.result === 'W') {
+    return foundItem.year
+    }
+}
